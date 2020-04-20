de43e5b79ad3e953e3cd74832bd7f12a02cde43a
peterem <emanuel.peter@hotmail.com>
2020-04-20 21:51:37 +0200

    estimate.cpp: replaced volume test Sphere with Ellipsoid

--- test/volume/estimate.cpp
+++ test/volume/estimate.cpp
@@ -11,17 +11,22 @@ extern "C" { // must be included C stlye
 #include "../../src/util/cli_functions.hpp"
 
 void test() {
-   Polytope* box = Polytope_new_box(4,2);
-   Polytope* box2 = Polytope_new_box(4,1);
+   const int n = 4;
+   
+   Polytope* box = Polytope_new_box(n,2);
+   Polytope* box2 = Polytope_new_box(n,1);
    Polytope_T.print(box);
    
-   FT s_center[4] = {0,0,0,0};
-   Sphere* s = Sphere_new(4,3,s_center);
-   Sphere_T.print(s);
+   Ellipsoid* e = Ellipsoid_new(n);
+   for(int i=0;i<n;i++) {
+      FT* Ai = Ellipsoid_get_Ai(e,i);
+      Ai[i] = 1.0/9.0;
+   }
+   Ellipsoid_T.print(e);
 
    // set up arrays to put in the sub-bodies:
-   void* body[2] = {box, s};
-   Body_T* type[2] = {&Polytope_T, &Sphere_T};
+   void* body[2] = {box, e};
+   Body_T* type[2] = {&Polytope_T, &Ellipsoid_T};
    
    void* body2[2] = {box, box2};
    Body_T* type2[2] = {&Polytope_T, &Polytope_T};
@@ -30,18 +35,18 @@ void test() {
       FT rx = 0.1*i;
       FT r0 = 1.5 - rx;
       FT r1 = 5.0 + rx;
-      FT vbox = volume_ref(4,r0,r1,  1,(const void**)&box,  (const Body_T**)&type);
-      FT vs   = volume_ref(4,r0,r1,  1,(const void**)&s,    (const Body_T**)&type[1]);
-      FT v    = volume_ref(4,r0,r1,  2,(const void**)&body, (const Body_T**)&type);
-      FT vbox2 = volume_ref(4,r0*0.5,r1,  2,(const void**)&body2, (const Body_T**)&type2);
+      FT vbox = volume_ref(n,r0,r1,  1,(const void**)&box,  (const Body_T**)&type);
+      FT vs   = volume_ref(n,r0,r1,  1,(const void**)&e,    (const Body_T**)&type[1]);
+      FT v    = volume_ref(n,r0,r1,  2,(const void**)&body, (const Body_T**)&type);
+      FT vbox2 = volume_ref(n,r0*0.5,r1,  2,(const void**)&body2, (const Body_T**)&type2);
       
       std::cout << "\nbox: " << vbox << " vs 256\n";
-      FT vs_ref = Ball_volume(4,3.0);
+      FT vs_ref = Ball_volume(n,3.0);
       std::cout << "sphere: " << vs << " vs " << vs_ref << "\n";
       std::cout << "intersection: " << v << "\n";
       std::cout << "\nbox2: " << vbox2 << " vs 16\n";
 
-      assert(std::abs(vbox - 256) <= 10); // Not great, want to get more accuracy
+      assert(std::abs(vbox - std::pow(4,n)) <= 10); // Not great, want to get more accuracy
       assert(std::abs(vs_ref - vs) <= 10);
       assert(vbox > v && vs > v);
       assert(std::abs(vbox2 - 16) <= 1.0);
@@ -49,7 +54,7 @@ void test() {
 
 
    Polytope_T.free(box);
-   Sphere_T.free(s);
+   Ellipsoid_T.free(e);
 }
 
 int main(int argc, char** argv) {
