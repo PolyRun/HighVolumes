e1ea3402d01df3ab60808f17f3e013e2edd9e51b
silaeubl <silaeubl@student.ethz.ch>
2020-04-23 09:37:21 +0200

    removed benchmark_cli base class, Made TSC timer standard

--- benchmark/benchmark.hpp
+++ benchmark/benchmark.hpp
@@ -21,7 +21,7 @@
 class Benchmark_base {
     public:
     Benchmark_base(std::string name_, int reps_, bool convergence_, int warmup_reps_) : name(name_), reps(reps_), convergence(convergence_), warmup_reps(warmup_reps_) {
-        timer = new Timer();
+        timer = new Tsc();
     }
 
     // overloaded constructer taking timer argument
@@ -123,129 +123,6 @@ class Benchmark_base {
         int warmup_reps;
 };
 
-/**
- * Base class for benchmarking functions that use cli (abstract)
- **/
-class Benchmark_base_cli : public Benchmark_base{
-    public:
-    Benchmark_base_cli(std::string name_, int reps_, bool convergence_, int warmup_reps_, CLIFunctions &cliFun_, bool benchmark_all_) : Benchmark_base(name_, reps_, convergence_, warmup_reps_), cliFun(cliFun_), benchmark_all(benchmark_all_){
-        }
 
-        virtual double run_benchmark() {
-            for (int j = 0; j < get_nr_functions(); ++j){
-                double min_time = std::numeric_limits<double>::max();
-                double max_time = -1;
-                double mean_time;
-                double std_dev = 0.0;
-                double total_time = 0;
-                double measured_times[reps];
-
-                double results[reps];
-                double results_sum = 0.0;
-
-                // Initialize
-                initialize();
-                select(j);
-                for (int i = 0; i < warmup_reps; ++i) {
-                    if (benchmark_all) {
-                        run_selected();
-                    } else {
-                        run();
-                    }
-                }
-
-                for (int i = 0; i < reps; ++i) {
-                    double result;
-                    // Run benchmark
-                    if (benchmark_all) {
-                        timer->start();
-                        result = run_selected();
-                        timer->stop();
-                    } else {
-                        timer->start();
-                        result = run();
-                        timer->stop();
-                    }
-
-                    results[i] = result;
-                    results_sum += result;
-
-                    measured_times[i] = timer->get_time();            
-                    total_time += measured_times[i];
-                    min_time = std::min(min_time, measured_times[i]);
-                    max_time = std::max(max_time, measured_times[i]);
-
-                    // Reset
-                    reset();
-
-                }
-                
-                // process time:
-                mean_time = total_time/reps;
-                for (int i = 0; i < reps; ++i) {
-                    std_dev += pow(measured_times[i] - mean_time, 2.0);
-                }
-                std_dev = sqrt(std_dev/reps);
-
-
-                // Dictionary output
-                std::cout << "{'time': {";
-                
-            
-                if (benchmark_all) {
-                    std::cout << "'name_t': '"<< name_selected << "', 'mean': '" << mean_time << "', 'min': '" << min_time << "', 'max': '" << max_time << "', 'std_dev': '" << std_dev << "'";
-                } else {
-                    std::cout << "'name_t': '"<< name << "', 'mean': '" << mean_time << "', 'min': '" << min_time << "', 'max': '" << max_time << "', 'std_dev': '" << std_dev << "'";
-                }
-                std::cout << "}, 'convergence': {";
-
-                // process results:
-                if(convergence) {
-                    double results_mean = results_sum/reps;
-                    double results_min = results[0];
-                    double results_max = results[0];
-                    double results_std_dev = 0;
-            
-                    for (int i = 0; i < reps; ++i) {
-                        results_std_dev += pow(results[i] - results_mean, 2.0);
-                        results_max = std::max(results_max, results[i]);
-                        results_min = std::min(results_min, results[i]);
-                    }
-                    results_std_dev = sqrt(results_std_dev/reps);
-                    
-                    if (benchmark_all) {
-                        std::cout << "'name_c': '"<< name_selected << "', 'mean': '" << results_mean << "', 'min': '" << results_min << "', 'max': '" << results_max << "', 'std_dev': '" << results_std_dev << "'";
-                        std::cout << "}}" << std::endl;
-                    } else {
-                        std::cout << "'name_c': '"<< name << "', 'mean': '" << results_mean << "', 'min': '" << results_min << "', 'max': '" << results_max << "', 'std_dev': '" << results_std_dev << "'";
-                        std::cout << "}}" << std::endl;
-                        break;
-                    }
-                } else {
-                    std::cout << "}}" << std::endl;
-                }
-            }
-        }
-
-    protected:
-        /**
-         * Returns the number of functions that are available
-         **/
-        virtual int get_nr_functions() = 0;
-
-        /**
-         * Selects the function that should be run
-         **/
-        virtual void select(int s) = 0;
-
-        /**
-         * Runs the selected function (only function call to prevent overhead)
-         **/
-        virtual double run_selected() = 0;
-
-        CLIFunctions cliFun;
-        bool benchmark_all; // If set, all available functions will be benchmarked, otherwise only the one selected by cli
-        std::string name_selected; // Name of the currently selected function
-};
 
 #endif // BENCHMARK_H
