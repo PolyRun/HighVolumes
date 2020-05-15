10ab110e55d2b15386c7f1937fe803be615c6d52
peterem <emanuel.peter@hotmail.com>
2020-05-15 17:59:42 +0200

    Fix bad merge conflict

--- src/volume/volume_helper.hpp
+++ src/volume/volume_helper.hpp
@@ -147,12 +147,8 @@ public:
                         {"cached_ref",        {{PolytopeT_intersectCoord_cached_ref, {PolytopeT_cacheReset_ref,PolytopeT_cacheUpdateCoord_ref}}, "no cache (ref)"}},
                         {"cached_nc1",        {{PolytopeT_intersectCoord_cached_nc1, {PolytopeT_cacheReset_ref,PolytopeT_cacheUpdateCoord_ref}}, "with cache, no condition - failed though"}},
                         {"cached_b_ref",        {{PolytopeT_intersectCoord_cached_b_ref, {PolytopeT_cacheReset_b_ref,PolytopeT_cacheUpdateCoord_b_ref}}, "with cache, b in cache (ref)"}},
+                        {"cached_vectorized", {{PolytopeT_intersectCoord_vectorized, {PolytopeT_cacheReset_ref,PolytopeT_cacheUpdateCoord_ref}}, "with cache and vectorized"}},
 		       	}));
-      add(new CLIF_Option<intersectCoord_f_t>(&PolytopeT_T.intersectCoord,'f',"PolytopeT_intersectCoord","cached_ref", {
-                                                     {"ref",        {PolytopeT_intersectCoord_ref, "no cache (ref)"}},
-						     {"cached_nc1", {PolytopeT_intersectCoord_cached_nc1, "with cache, nc1."}},
-                       {"cached_vectorized", {PolytopeT_intersectCoord_vectorized, "with cache and vectorized"}},
-						     {"cached_ref", {PolytopeT_intersectCoord_cached_ref,"with cache (ref)"}} }));
 
       add(new CLIF_TrippleOption<intersectCoord_f_t, cacheReset_f_t, cacheUpdateCoord_f_t>
           (&PolytopeCSC_T.intersectCoord, &PolytopeCSC_T.cacheReset, &PolytopeCSC_T.cacheUpdateCoord,
